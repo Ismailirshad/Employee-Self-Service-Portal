53634deb8bf102751c8255e971c637c21809b5a0
Ismail Irshad <irshadsha164@gmail.com>
2026-03-01 09:30:50 +0530

    Initial Commit

---
+++ employee_self_service/employee_self_service/doctype/employee/employee.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Ismail Irshad and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.utils.nestedset import NestedSet
+
+
+class Employee(NestedSet):
+	pass
