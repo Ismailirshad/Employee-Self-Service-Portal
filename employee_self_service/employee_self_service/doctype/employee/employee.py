abeb302c70d8b91b3d1bef3785d2d515ecaa97e0
Ismail Irshad <irshadsha164@gmail.com>
2026-03-04 16:31:17 +0530

    updated doctype folder

--- employee_self_service/employee_self_service/doctype/employee/employee.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026, Ismail Irshad and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.utils.nestedset import NestedSet
-
-
-class Employee(NestedSet):
-	pass
--- employee_self_service/employee_self_service/doctype/shift_schedule/shift_schedule.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026, Ismail Irshad and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class ShiftSchedule(Document):
-	pass
